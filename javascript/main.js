a8023554a4ec2ef61b83cbbf6111ef17dbfa9cee
Jason Ng <S10262552@connect.np.edu.sg>
2023-11-15 20:03:22 +0800

    Slideshow Update 2
    -Added Slideshow CSS and JS
    -Updated Images in Slideshow

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,29 @@
-function myFunction() {
+  function myFunction() {
     var x = document.getElementById("myNavbar");
     if (x.className === "navbar") {
       x.className += " responsive";
-    } else {
+    } 
+    else {
       x.className = "navbar";
     }
+  }
+
+  let slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
   }
